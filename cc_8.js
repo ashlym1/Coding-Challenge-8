26fa8fb4f6f378750f249dceca462e247e4f7e42
ashlym1 <ashlym3@usf.edu>
2025-02-16 19:34:48 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -19,11 +19,23 @@ console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.
 console.log(calculateDiscount(250, 0.15));// Expected output: "Final Price: $212.50"
 
 // Task 3: Arrow Function 
-
-
-
-
-
+ const calculateServiceFee = (amount,servicesType)=> {
+    let feeRate;
+    if (serviceType ==="Premium") { 
+        feeRate= 0.15;// 15 % of the amount 
+     } else if (serviceType === "Standars" ){
+        feeRate= 0.10 ; // 10 % of the amount
+     } else if (serviceType=== "Basic") {
+        feeRate = 0.05; // 5% of the amount 
+     } else {
+    return "Invalid service Type"; // incase of any incorrect inputs 
+ }
+let serviceFee= amount * feeRate; // Calculating the service fee 
+return `Service Fee: $${serviceFee.Fee.toFixed(2)}`;
+ };
+// Test Data: 
+console.log(calculateServiceFee(200, "Premium")); //Expected output: "Service Fee: $30.00"
+console.log(calculateServiceFee(500, "Standard")); //Expected output: "Service Fee: $50.00"
 // Task 4: Parameters and Arguments 
 
 
