87fbb5ddc2ec114c09fb9f71f54c77162c33bef7
ashlym1 <ashlym3@usf.edu>
2025-02-16 19:49:22 -0500

    Task 4 - Car Rental Cost Calculation

--- cc_8.js
+++ cc_8.js
@@ -4,7 +4,7 @@ let taxAmount = baseSalary * taxRate; // calulating the tax amount based on the 
 let netSalary = (baseSalary + bonus)-taxAmount; // net salary + the bonus - tax
 return `netSalary: $${netSalary.toFixed(2)}`; // formating the net salary 
 }
-// Test Data
+    // Test Data:
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); //Expected output: "Net Salary: $6950.00"
 
@@ -14,7 +14,7 @@ const calculatedDiscount= function (price, discountRate) {
     let finalPrice= pricerice-discountAmount; // subtracting amount from the original price 
     return `Final Price : $${finalPrice.toFixed(2) }`;// returning the formatted final price
 };
-// Test Data
+    // Test Data :
 console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
 console.log(calculateDiscount(250, 0.15));// Expected output: "Final Price: $212.50"
 
@@ -33,12 +33,40 @@ console.log(calculateDiscount(250, 0.15));// Expected output: "Final Price: $212
 let serviceFee= amount * feeRate; // Calculating the service fee 
 return `Service Fee: $${serviceFee.Fee.toFixed(2)}`;
  };
-// Test Data: 
+    // Test Data: 
 console.log(calculateServiceFee(200, "Premium")); //Expected output: "Service Fee: $30.00"
 console.log(calculateServiceFee(500, "Standard")); //Expected output: "Service Fee: $50.00"
+
+
 // Task 4: Parameters and Arguments 
+function calculateRentalCost(days, carType, insurance = false) {
+    let dailyRate;
+
+    // Dertemining the cost depending on the car 
+    if (carType === "Economy") {
+        dailyRate = 40; // Economy costs 40 per day
+    } else if (carType === "Standard") {
+        dailyRate = 60; // Standard costs 60 per day
+    } else if (carType === "Luxury") {
+        dailyRate = 100; // Luxury costs 100 per day
+    } else {
+        return "Invalid car type"; // If car type isn't valid, it will return as error 
+    }
+
+    let totalCost = days * dailyRate;  // Calculating  the base cost
+
+    // Adding  insurance cost if applicable
+    if (insurance) {
+        totalCost += days * 20; // Insurance cost an extra 20 per day 
+    }
 
 
+    return `Total Rental Cost: $${totalCost.toFixed(2)}`;
+}     // total rental cost
+
+// Test Data : 
+console.log(calculateRentalCost(3, "Economy", true)); // Expected output: "Total Rental Cost: $180"
+console.log(calculateRentalCost(5, "Luxury", false)); // Expected output: "Total Rental Cost: $500"
 
 
 
@@ -48,7 +76,7 @@ console.log(calculateServiceFee(500, "Standard")); //Expected output: "Service F
 
 
 
-
+ // Test Date 
 
 
 // Task 6- Higher-order function
@@ -56,11 +84,16 @@ console.log(calculateServiceFee(500, "Standard")); //Expected output: "Service F
 
 
 
-
+  // Test Data :
 
 // Task 7- Closure 
 
 
-
+ // Test Data : 
 
 // Task 8: Recursion in JavaScript 
+
+
+
+
+ // Test Data: 
