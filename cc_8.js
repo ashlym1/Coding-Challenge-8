c0b03483c5b9c243921ff83bf42bd07dfb88e33b
ashlym1 <ashlym3@usf.edu>
2025-02-16 19:22:30 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -2,16 +2,21 @@
 function calculateSalary (baseSalary,bonus, taxRate) {
 let taxAmount = baseSalary * taxRate; // calulating the tax amount based on the salary 
 let netSalary = (baseSalary + bonus)-taxAmount; // net salary + the bonus - tax
-return `netSalary: $ ${netSalary.toFixed(2)}`; // formating the net salary 
+return `netSalary: $${netSalary.toFixed(2)}`; // formating the net salary 
 }
 // Test Data
 console.log(calculateSalary(5000, 500, 0.1)); // Expected output: "Net Salary: $5000.00"
 console.log(calculateSalary(7000, 1000, 0.15)); //Expected output: "Net Salary: $6950.00"
 
 // Task 2- Function Expression 
-
-
-
+const calculatedDiscount= function (price, discountRate) {
+    let discountAmount= price * discountRate; // calculating the discount amount 
+    let finalPrice= pricerice-discountAmount; // subtracting amount from the original price 
+    return `Final Price : $${finalPrice.toFixed(2) }`;// returning the formatted final price
+};
+// Test Data
+console.log(calculateDiscount(100, 0.2)); // Expected output: "Final Price: $80.00"
+console.log(calculateDiscount(250, 0.15));// Expected output: "Final Price: $212.50"
 
 // Task 3: Arrow Function 
 
